65b4580525cefbaedccddb266ef073bd32302189
Anushree Mehta <anushreemehta6@gmail.com>
2026-05-21 22:41:56 +0530

    pata nahi bhai

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 
-import { div } from "motion/react-client";
+// import { div } from "motion/react-client";
 import Image from "next/image";
 import Dashboard from "./dashboard/page";
 import HomePage from "@/pages/HomePage";
